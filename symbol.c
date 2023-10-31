e17409a923a2745e19129e39d5af97374929c7a6
Ka-rt <146866213+Ka-rt@users.noreply.github.com>
2023-11-01 00:45:40 +0530

    Create symbol.c

---
+++ symbol.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+struct student{
+        int roll;
+        char name[];
+}student1;
+void display();
+int main() {
+        int a = 10;
+        float b = 3.14;
+        char c = 'S';
+        char name[];
+        int int;
+        int for;
+        int if;
+        int while;
+        scanf("%d",a);
+        printf("Hello, World!");
+        return 0;
+}
